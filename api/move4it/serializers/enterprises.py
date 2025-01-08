13ddf2c4aac859da9a5fbd55782896841f332d27
Felipe Barraza <felipe.barraza.vega@gmail.com>
2025-01-08 13:00:56 -0300

    avance de restructuracion y serailizacion comprimida a las necesidades

--- api/move4it/serializers/enterprises.py
+++ api/move4it/serializers/enterprises.py
@@ -1,23 +1,43 @@
 from django.db.models.base import Model
 from rest_framework import serializers
-from api.move4it.models import Enterprise, Group, Competence, Interval, RegisterActivity
+from api.move4it.models import Enterprise, Group, Competence, Interval, RegisterActivity, FileRegisterActivity
+from api.users.models import User
 
-class RegisterActivitySerializer(serializers.Serializer):
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'first_name', 'last_name', 'email', 'is_active',)
+class FileRegisterActivitySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = FileRegisterActivity
+        fields = '__all__'
+
+class RegisterActivitySerializer(serializers.ModelSerializer):
+    files = serializers.SerializerMethodField('get_files')
+    users = UserSerializer(many=True)
+    
+    def get_files(self, register_activity):
+        files = FileRegisterActivity.objects.filter(register_activity=register_activity).all()
+        serialized_files = FileRegisterActivitySerializer(files, many=True).data
+        return serialized_files
     class Meta:
         model = RegisterActivity
-        fields =('__all__')
+        fields = ('id', 'start_date_time', 'finish_date_time', 'location', 'value', 'is_active','is_load','is_completed', 'observation', 'location', 'files','users')
+        depth = 3
 
 class IntervalSerializer(serializers.ModelSerializer):
-    activities = serializers.SerializerMethodField('get_activities')
+    assignations = serializers.SerializerMethodField('get_activities')
     
     def get_activities(self, interval):
-        activity = RegisterActivity.objects.filter(interval=interval.id).all()
-        serialized_activities = RegisterActivitySerializer(activity, many=True).data
-        return serialized_activities
+        activities = RegisterActivity.objects.filter(interval=interval).all()
+        serialized_activities = RegisterActivitySerializer(activities, many=True).data
+        return serialized_activities 
 
     class Meta:
         model = Interval
-        fields = ('start_date', 'end_date', 'activities')
+        fields = ('start_date', 'end_date', 'assignations')
+        depth = 3
 
 class CompetenceSerializer(serializers.ModelSerializer):
 
@@ -32,6 +52,7 @@ class CompetenceSerializer(serializers.ModelSerializer):
     class Meta:
         model = Competence
         fields = '__all__'
+        depth = 2
 
 
 class EnterpriseSerializer(serializers.ModelSerializer):
